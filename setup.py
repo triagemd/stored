11a6ca7d231465c881d4ea957bc51a0f15bf42a2
Steven Normore <snormore@gmail.com>
2017-09-04 22:54:15 -0400

    Clean up gcloud auth

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='stored',
-    version='0.0.16',
+    version='0.0.19',
     description='Manage files and directories on different storage backends.',
     author='Triage Technologies Inc.',
     author_email='ai@triage.com',
--- stored/list_files.py
+++ stored/list_files.py
@@ -1,8 +1,6 @@
-from .auth import with_backend_auth
 from .backends import get_backend
 
 
-@with_backend_auth
 def list_files(path, relative=True):
     storage = get_backend(path)
     return storage.list(relative=relative)
